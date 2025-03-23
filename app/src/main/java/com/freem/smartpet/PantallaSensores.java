620b99df14138b1c58de17296cbc0b2bc8eb727f
RVCH-77 <riveravalderramachristopherisa@gmail.com>
2025-03-22 20:03:40 -0600

    añadi una nueva dependencia

--- app/src/main/java/com/freem/smartpet/PantallaSensores.java
+++ app/src/main/java/com/freem/smartpet/PantallaSensores.java
@@ -1,6 +1,7 @@
 package com.freem.smartpet;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class PantallaSensores extends AppCompatActivity {
 
+    private TextView txtTemperatura;
+    private TextView txtHumedad;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pantalla_sensores);
+
+        txtTemperatura = findViewById(R.id.txtTemperatura);
+        txtHumedad = findViewById(R.id.txtHumedad);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
